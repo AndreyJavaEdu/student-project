488f149eabb239f18bdc56de9dd7dab8446c7565
AndreyJavaEd <scream123456@yandex.ru>
2023-07-19 18:07:27 +0300

    StudentOrder - добавили список детей используя коллекцию (детей может быть больше одного). Добавили Геттер для коллекции и реализовали вставку элемента Child child в коллекцию. SaveStudentOrder - buildStudentOrder: создали двух детей и добавили в нашу завку методом addChild. CityRegisterValidator - checkCityRegister: реализовали перебор трем способами коллекции ArrayList детей, для проверки каждого ребенка.

--- src/edu/pet_project/studentorder/validator/CityRegisterValidator.java
+++ src/edu/pet_project/studentorder/validator/CityRegisterValidator.java
@@ -1,9 +1,13 @@
 package edu.pet_project.studentorder.validator;
 
 import edu.pet_project.studentorder.domain.AnswerCityRegister;
+import edu.pet_project.studentorder.domain.Child;
 import edu.pet_project.studentorder.domain.CityRegisterCheckerResponse;
 import edu.pet_project.studentorder.domain.StudentOrder;
 import edu.pet_project.studentorder.exception.CityRegisterException;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class CityRegisterValidator {
 
@@ -24,7 +28,20 @@ public class CityRegisterValidator {
         try {
             CityRegisterCheckerResponse hans = personChecker.checkPerson(so.getHusband()); // в студ заявлении есть муж
             CityRegisterCheckerResponse wans = personChecker.checkPerson(so.getWife());
-            CityRegisterCheckerResponse cans = personChecker.checkPerson(so.getChild());
+            //1 способ пройтись по всем элементам по номеру
+            List<Child> children = so.getChildren();
+            for(int i = 0; i< children.size(); i++) {
+                CityRegisterCheckerResponse cans = personChecker.checkPerson(children.get(i));
+            }
+            //2 способ пробежаться по коллекции с помощью Итератора
+            for (Iterator<Child> it = children.iterator(); it.hasNext(); ){
+                Child child = it.next();
+                CityRegisterCheckerResponse cans = personChecker.checkPerson(child);
+            }
+            //3 способ for each loop
+            for (Child child: children) {
+                CityRegisterCheckerResponse cans = personChecker.checkPerson(child);
+            }
         } catch (CityRegisterException ex) {
             ex.printStackTrace(System.out);
         }
