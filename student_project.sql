b24b0a0e327a03db8760ae9014555c5973111963
AndreyJavaEd <scream123456@yandex.ru>
2023-07-21 00:20:33 +0300

    Дополнили таблицами базу: jc_student_child Добавили колонки в таблицу jc_student_order: h_pasport_seria varchar(10) not null, h_passport_number varchar(10) not null, h_passport_date date not null, h_passport_office_id integer not null,

--- student_project.sql
+++ student_project.sql
@@ -1,5 +1,7 @@
-DROP TABLE if exists jc_register_office;
+DROP TABLE if exists jc_student_child;
+DROP TABLE if exists jc_student_order;
 DROP TABLE if exists jc_passport_office;
+DROP TABLE if exists jc_register_office;
 DROP TABLE if exists jc_country_struct;
 DROP TABLE if exists jc_street;
 
@@ -15,7 +17,6 @@ create table jc_country_struct
     area_id char(12) not null,
     area_name varchar(200),
     PRIMARY KEY(area_id)
-
 );
 
 create table jc_passport_office
@@ -24,8 +25,7 @@ create table jc_passport_office
     p_office_area_id char(12),
     p_office_name varchar(200),
     PRIMARY KEY(p_office_id),
-    FOREIGN KEY (p_office_area_id)
-        references jc_country_struct(area_id) ON DELETE RESTRICT
+    FOREIGN KEY (p_office_area_id) references jc_country_struct(area_id) ON DELETE RESTRICT
 );
 
 create table jc_register_office
@@ -40,12 +40,15 @@ create table jc_register_office
 
 create table jc_student_order
 (
-
     student_order_id SERIAL,
     h_surname varchar(100) not null,
     h_given_name varchar(100) not null,
     h_patronomyc varchar(100) not null,
     h_date_of_birth date not null,
+    h_pasport_seria varchar(10) not null,
+    h_passport_number varchar(10) not null,
+    h_passport_date date not null,
+    h_passport_office_id integer not null,
     h_post_index varchar(10),
     h_street_code integer not null,
     h_building varchar(10) not null,
@@ -56,6 +59,10 @@ create table jc_student_order
     w_given_name varchar(100) not null,
     w_patronomyc varchar(100) not null,
     w_date_of_birth date not null,
+    w_pasport_seria varchar(10) not null,
+    w_passport_number varchar(10) not null,
+    w_passport_date date not null,
+    w_passport_office_id integer not null,
     w_post_index varchar(10),
     w_street_code integer not null,
     w_building varchar(10) not null,
@@ -69,5 +76,26 @@ create table jc_student_order
     FOREIGN KEY (h_street_code) references jc_street(street_code) ON DELETE RESTRICT,
     FOREIGN KEY (w_street_code) references jc_street(street_code) ON DELETE RESTRICT,
     FOREIGN KEY (register_office_id) references jc_register_office(r_office_id) ON DELETE RESTRICT
+);
 
-)
+create table jc_student_child
+(
+    student_child_id SERIAL,
+    student_order_id integer not null,
+    с_surname varchar(100) not null,
+    с_gсiven_name varchar(100) not null,
+    с_patronomyc varchar(100) not null,
+    с_date_of_birth date not null,
+    с_certificate_number varchar(10) not null,
+    с_certificate_date date not null,
+    с_register_office_id integer not null,
+    с_post_index varchar(10),
+    с_street_code integer not null,
+    с_building varchar(10) not null,
+    с_extension varchar(10),
+    с_apartment varchar(10),
+    PRIMARY KEY (student_child_id),
+    FOREIGN KEY (с_street_code) references jc_street (street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (с_register_office_id) references jc_register_office (r_office_id) ON DELETE RESTRICT,
+    FOREIGN KEY (student_order_id) references jc_student_order(student_order_id) ON DELETE RESTRICT
+);
